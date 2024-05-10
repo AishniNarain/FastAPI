a92c08acf723de57fb6219f63dba4457296d007a
Aishni Narain <aishninarain@gmail.com>
2024-05-10 16:27:10 +0530

    Added files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI, Path, Query # type: ignore
 from pydantic import BaseModel  # type: ignore
 
 app = FastAPI()
@@ -22,14 +22,30 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
+# #Path parameters
+# @app.get("/hello/{name}/{age}")
+# def hello(name:str, age:int):
+#     return {"name": name,
+#             "age": age
+#             }
+
+#Query parameters
+# @app.get("/hello")
+# def hello(name:str, age:int):
+#     return {"name": name,
+#             "age": age
+#             }
+    
+#Path parameters with validations
 @app.get("/hello/{name}/{age}")
-def hello(name:str, age:int):
+def hello(name:str = Path(...,min_length=3,max_length =10), age:int = Path(...,ge=1,le=100)):
     return {"name": name,
             "age": age
             }
     
+#Query parameters with validations
 @app.get("/hello")
-def hello(name:str, age:int):
+def hello(name:str, age:int,percent:float=Query(..., ge=20, le=100)):
     return {"name": name,
             "age": age
             }
